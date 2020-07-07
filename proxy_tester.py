16560e05ec72c28cf4825cade615e5f7b5c5216f
Shlomi Kushchi <shlomik@gmail.com>
2020-07-07 17:17:22 +0300

    using async with for the client connection

--- proxy_tester.py
+++ proxy_tester.py
@@ -12,44 +12,43 @@ import websockets
 
 async def register():
     uri = "ws://localhost:8765/test"
-    ws = await websockets.connect(uri)
+    async with websockets.connect(uri) as ws:
+        await ws.send(json.dumps({
+            'action': 'authenticate',
+            'data':   {
+                'key_id':     cred['key_id'],
+                'secret_key': cred['secret_key'],
+            }
+        }))
 
-    await ws.send(json.dumps({
-        'action': 'authenticate',
-        'data':   {
-            'key_id':     cred['key_id'],
-            'secret_key': cred['secret_key'],
-        }
-    }))
+        response = json.loads(await ws.recv())
+        print(response)
 
-    response = json.loads(await ws.recv())
-    print(response)
+        await ws.send(json.dumps({
+            'action': 'listen',
+            'data':   {
+                'streams': ["alpacadatav1/Q.AAPL"],
+            }
+        }))
 
-    await ws.send(json.dumps({
-        'action': 'listen',
-        'data':   {
-            'streams': ["alpacadatav1/Q.AAPL"],
-        }
-    }))
+        get_aapl = False
+        while 1:
+            try:
+                response = await ws.recv()
+                print(json.loads(response))
+                await ws.send(json.dumps({
+                    'action': 'listen',
+                    'data':   {
+                        'streams': ["alpacadatav1/Q.{}".format("AA" if get_aapl
+                                                               else "TSLA")],
+                    }
+                }))
+                get_aapl = not get_aapl
+            except Exception as e:
+                print(e)
+            await asyncio.sleep(0.5)
 
-    get_aapl = False
-
-    while 1:
-        try:
-            response = (await ws.recv())
-            print(json.loads(response))
-            await ws.send(json.dumps({
-                'action': 'listen',
-                'data':   {
-                    'streams': ["alpacadatav1/Q.{}".format("AA" if get_aapl else "TSLA")],
-                }
-            }))
-            get_aapl = not get_aapl
-        except Exception as e:
-            print(e)
-        await asyncio.sleep(1)
-
-    # await ws.close()
+        # await ws.close()
 
 asyncio.get_event_loop().run_until_complete(register())
 asyncio.get_event_loop().run_forever()
