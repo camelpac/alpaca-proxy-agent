b831c510ce963ccacfb0a79298c7cbf3d6198ea6
Shlomi Kushchi <shlomik@gmail.com>
2020-08-11 10:03:36 +0300

    change ip for docker execution docker doesn't know what is localhost

--- main.py
+++ main.py
@@ -146,7 +146,7 @@ if __name__ == '__main__':
     # asyncio.get_event_loop().run_until_complete(b())
     # asyncio.get_event_loop().run_forever()
     #
-    start_server = websockets.serve(serve, "localhost", 8765)
+    start_server = websockets.serve(serve, "0.0.0.0", 8765)
 
     asyncio.get_event_loop().run_until_complete(start_server)
     asyncio.get_event_loop().run_forever()
